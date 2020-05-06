500a4d5ac2ce02e154fae33ccfe7e7d97d1c5632
Reidylla Lima <reidylla.lima@pop-pe.rnp.br>
2020-05-06 20:48:32 -0300

    variável LET

---
+++ aula6/index.js
@@ -0,0 +1,14 @@
+//Duas maneiras de criar variáveis
+//Com let, var e const
+
+let nome = 'João';
+
+console.log(nome, 'nasceu em 1984');
+
+// Posso criar uma variável e não incializá-la
+
+let idade; // Declarei
+
+idade = 14; // Inicializei
+
+console.log(idade);
